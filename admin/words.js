e2c9d02bc8bacb1ce5a5724c36f79d729886fbe5
ALFOLIcd <o.alff@mailbox.org>
2021-12-14 20:27:49 +0100

    initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+	'mariadb-sql adapter settings': {
+		'en': 'Adapter settings for mariadb-sql',
+		'de': 'Adaptereinstellungen für mariadb-sql',
+		'ru': 'Настройки адаптера для mariadb-sql',
+		'pt': 'Configurações do adaptador para mariadb-sql',
+		'nl': 'Adapterinstellingen voor mariadb-sql',
+		'fr': "Paramètres d'adaptateur pour mariadb-sql",
+		'it': "Impostazioni dell'adattatore per mariadb-sql",
+		'es': 'Ajustes del adaptador para mariadb-sql',
+		'pl': 'Ustawienia adaptera dla mariadb-sql',
+		'zh-cn': 'mariadb-sql的适配器设置'
+	},
+	'option1': {
+		'en': 'option1',
+		'de': 'Option 1',
+		'ru': 'Опция 1',
+		'pt': 'Opção 1',
+		'nl': 'Optie 1',
+		'fr': 'Option 1',
+		'it': 'opzione 1',
+		'es': 'Opción 1',
+		'pl': 'opcja 1',
+		'zh-cn': '选项1'
+	},
+	'option2': {
+		'en': 'option2',
+		'de': 'Option 2',
+		'ru': 'option2',
+		'pt': 'opção 2',
+		'nl': 'Optie 2',
+		'fr': 'Option 2',
+		'it': 'opzione 2',
+		'es': 'opcion 2',
+		'pl': 'Opcja 2',
+		'zh-cn': '选项2'
+	}
+};
